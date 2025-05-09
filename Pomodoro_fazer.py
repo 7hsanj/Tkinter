c3a9b1d8530a92f10790cfa02344370d7e720eab
Jose <Jmcnash0605@gmail.com>
2025-05-09 17:57:51 +0100

    Escolher os tempos

--- Pomodoro_fazer.py
+++ Pomodoro_fazer.py
@@ -1,4 +1,5 @@
-# Escolher tempos
+# Escolher tempos (FEITO)
+# Fazer com que mudar o tempo apenas afete diferença e não dar só set
 # Mostrar tempo total trabalhado
 # Mostrar tempo por disciplina
 # Matplotlib para guardar tempo trabalhado ao longo de dias, etc.
@@ -10,42 +11,131 @@ from ttkbootstrap import ttk, Style
 
 class Pomodoro:
     def __init__(self):
+
         self.root = tk.Tk()
         self.root.geometry("400x300")
         self.root.title("Pomodoro Timer")
 
+        # teste = tk.IntVar(self.root, value=15)
+        # print(teste.get())
+
         self.style = Style(theme="morph")
         self.style.theme_use()
 
-        self.temporizador_ativo = True
-        self.tempo_de_trabalho = 25*60
-        self.tempo_de_pausa_curta = 5*60
-        self.tempo_de_pausa_longa = 15*60
+        self.intvar_trabalho = tk.IntVar(value=25)
+        self.intvar_pausa_curta = tk.IntVar(value=5)
+        self.intvar_pausa_longa = tk.IntVar(value=15)
 
-        self.pomodoro_feitos = 1
+        self.tempo_atual = self.intvar_trabalho.get()*60
+        self.minutos = self.tempo_atual//60
+        self.segundos = self.tempo_atual-self.minutos*60
+        # print(self.tempo_atual)
+
+        self.main = tk.Frame(self.root)
+        self.escolher = tk.Frame(self.root)
 
         self.pausa = False
+        self.pomodoro_feitos = 1
 
-        self.tempo_atual = self.tempo_de_trabalho
+        self.Criar_Widgets()
+        self.Tempo_escolhido_trabalho()
+        self.Tempo_escolhido_curta()
+        self.Tempo_escolhido_longa()
 
-        self.Mostrar_tempo()
+        self.intvar_trabalho.trace_add('write', self.Tempo_escolhido_trabalho)
+        self.intvar_pausa_curta.trace_add('write', self.Tempo_escolhido_curta)
+        self.intvar_pausa_longa.trace_add('write', self.Tempo_escolhido_longa)
+
+        self.main.pack()
+
+        self.root.mainloop()
+
+    def Criar_Widgets(self):
+        # Main
         self.temporizador = tk.Label(
-            self.root, text='{0}:{1}'.format(self.minutos, self.segundos), font=('Arial', 80))
+            self.main, text='{0}:{1}'.format(self.minutos, self.segundos), font=('Arial', 80))
         self.temporizador.pack(pady=5)
 
         self.start_button = ttk.Button(
-            self.root, text="Iniciar tempo", command=self.Iniciar_tempo)
+            self.main, text="Iniciar tempo", command=self.Iniciar_tempo)
         self.start_button.pack(pady=5)
 
         self.stop_button = ttk.Button(
-            self.root, text="Parar tempo", command=self.Parar_tempo, state=tk.DISABLED)
+            self.main, text="Parar tempo", command=self.Parar_tempo, state=tk.DISABLED)
         self.stop_button.pack(pady=5)
 
-        self.root.mainloop()
+        self.choose_button = ttk.Button(
+            self.root, text='Escolher tempo', command=self.Escolher_tempo)
+        self.choose_button.place(relx=0.8, rely=0.9, anchor='center')
+
+        # Escolhas
+        self.label_escolher = tk.Label(
+            self.escolher, text='Personaliza os teus tempos!', font=('Arial', 20))
+        self.label_escolher.pack(pady=5)
+
+        self.titulo_trabalho = tk.Label(
+            self.escolher, text='Tempo de Trabalho', font=('Arial', 10))
+        self.titulo_trabalho.pack(pady=5)
+
+        self.entry_tempo_de_trabalho = tk.Entry(
+            self.escolher, textvariable=self.intvar_trabalho)
+        self.entry_tempo_de_trabalho.pack()
+
+        self.titulo_curta = tk.Label(
+            self.escolher, text='Tempo da Pausa', font=('Arial', 10))
+        self.titulo_curta.pack(pady=5)
+
+        self.entry_tempo_de_pausa_curta = tk.Entry(
+            self.escolher, textvariable=self.intvar_pausa_curta)
+        self.entry_tempo_de_pausa_curta.pack()
+
+        self.titulo_longa = tk.Label(
+            self.escolher, text='Tempo da Grande Pausa', font=('Arial', 10))
+        self.titulo_longa.pack(pady=5)
+
+        self.entry_tempo_de_pausa_longa = tk.Entry(
+            self.escolher, textvariable=self.intvar_pausa_longa)
+        self.entry_tempo_de_pausa_longa.pack()
+
+        self.back_button = ttk.Button(
+            self.root, text='Voltar', command=self.Voltar_Pomodoro)
+
+    def Escolher_tempo(self):
+        self.main.pack_forget()
+        self.choose_button.place_forget()
+        self.back_button.place(relx=0.8, rely=0.9, anchor='center')
+
+        self.escolher.pack()
+
+    def Voltar_Pomodoro(self):
+        self.main.pack()
+        self.escolher.pack_forget()
+        self.back_button.place_forget()
+        self.choose_button.place(relx=0.8, rely=0.9, anchor='center')
+
+    def Tempo_escolhido_trabalho(self, *args):
+        self.tempo_de_trabalho = self.intvar_trabalho.get()*60
+        if not self.pausa:
+            self.tempo_atual = self.tempo_de_trabalho
+        self.Mostrar_tempo()
+
+    def Tempo_escolhido_curta(self, *args):
+        self.tempo_de_pausa_curta = self.intvar_pausa_curta.get()*60
+        if self.pausa and not self.pomodoro_feitos == 4:
+            self.tempo_atual = self.tempo_de_pausa_curta
+        self.Mostrar_tempo()
+
+    def Tempo_escolhido_longa(self, *args):
+        self.tempo_de_pausa_longa = self.intvar_pausa_longa.get()*60
+        if self.pausa and self.pomodoro_feitos == 1:
+            self.tempo_atual = self.tempo_de_pausa_longa
+        self.Mostrar_tempo()
 
     def Mostrar_tempo(self):
         self.minutos = self.tempo_atual//60
         self.segundos = self.tempo_atual-self.minutos*60
+        self.temporizador.config(text='{0}:{1}'.format(
+            self.minutos, self.segundos), font=('Arial', 80))
 
     def Parar_tempo(self):
         self.stop_button.config(state=tk.DISABLED)
@@ -65,9 +155,7 @@ class Pomodoro:
         if self.temporizador_ativo:
             self.tempo_atual -= 1
             self.Mostrar_tempo()
-            self.temporizador.config(text='{0}:{1}'.format(
-                self.minutos, self.segundos), font=('Arial', 80))
-            self.root.after(1000, self.Atualizar_tempo)
+            self.root.after(1, self.Atualizar_tempo)
 
     def Fim_de_tempo(self):
         if self.pausa:
